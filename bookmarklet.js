fef534b44d5d4b91d2b6ae2c49d230c546aeac0b
Parker Phinney <parker.phinney@gmail.com>
2012-01-31 23:24:25 -0500

    set up app structure. wrote README with design speck/ advice for hackathon

--- bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://code.jquery.com/jquery-1.5.1.min.js';document.body.appendChild(document.createElement('script')).src='http://hacktown.cs.dartmouth.edu/ddstown/injection.js';})();
