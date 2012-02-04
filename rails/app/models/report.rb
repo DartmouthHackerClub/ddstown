862e37799fad93f1ed8299e8667eca9ad35468e9
Marco A. Peraza <marcoaperaza@gmail.com>
2012-02-04 15:44:17 -0500

    started working on parsing the html reports

--- rails/app/models/report.rb
+++ rails/app/models/report.rb
@@ -2,7 +2,19 @@ class Report < ActiveRecord::Base
 	belongs_to :user
 
   def parse
-    
+    doc = Nokogiri::HTML(text)
+
+    doc.css('.Data > tr').each do |row|
+      data = row.text.split(/\r\n\t+/m)
+      time, location, plan, kind, amount = data[0], data[1], data[3], data[6], data[7]
+
+      next if location !~ /Novack|Collis|Commons|Courtyard|King/i
+
+      if plan =~ /SmartChoice/
+        user.create_swipe(:location => location, :time => )
+    end
+
   end
+
   handle_asynchronously :parse
 end
--- rails/db/migrate/20120204182854_create_reports.rb
+++ rails/db/migrate/20120204182854_create_reports.rb
@@ -2,7 +2,6 @@ class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
       t.integer :user_id
-
       t.text    :html
       
       t.timestamps
