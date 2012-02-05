09ae56c6422ec1d9239c6f378d39c1be625f7904
Zach Denton <zacharydenton@gmail.com>
2012-02-04 21:13:26 -0500

    report#show

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -4,7 +4,11 @@ class ReportsController < ApplicationController
   end
 
   def show
-
+    @report = Report.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @report }
+    end
   end
 
   def create
