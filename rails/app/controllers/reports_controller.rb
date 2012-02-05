3c9fdae49f95fa4ef6d317769cdc94b2b51eed7b
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-02-04 21:56:01 -0500

    fixes for bookmarklet integration

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -8,12 +8,16 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @report = current_user.reports.build(params[:report])
+    puts "creating"
+    @report = current_user.reports.build()
+    @report.html = params[:transaction_history_html]
     @report.user = current_user
     if @report.save
+      puts "it saved"
       @report.parse
       redirect_to :controller => :swipes, :action => :index
     else
+      puts "it didn't save"
       render :action => :new
     end
   end
