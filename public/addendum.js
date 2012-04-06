363d703a65f56504be55180522217dc79dca9dc5
Parker Phinney <parker.phinney@gmail.com>
2012-04-06 18:45:07 -0400

    reorganized addendum js file into logical blocks with labels

--- public/addendum.js
+++ public/addendum.js
@@ -27,32 +27,10 @@ var current_balance_page_html_ready = false;
 var transaction_history_page_html_ready = false;
 var banner_page_html_ready = false;
 
-// sends the user to the given url, with given data in post
-function send_user_by_post(url, data){
-    form = $('<form method="post" action="' + url + '"></form>');
-    for (var name in data){
-        value = data[name];
-        value = Base64.encode(value);
-        form.append($('<input type="hidden" name="' + name + '" value="' + value + '">'));
-    }
-    form.submit();
-}
 
-// see if an html str is the login page
-function is_login_page(html){
-    return html.indexOf('Forgot My Password') >= 0;
-}
-
-function handle_they_must_log_in(){
-    alert("Woops! You're not logged in. Redirecting you to login.");
-    window.location.href = managemyid_login_page;
-}
-
-function handle_error_getting_html(page, e){
-    error_message_str = e.statusText + ' ' + String(e.status);
-    alert("Error getting " + page + " page! Super lame. Sorry. " + error_message_str);
-    console.log(e);
-}
+////////////////////////////////////////////////////////////////
+// AJAX REQUESTS FOR HTML PAGES
+////////////////////////////////////////////////////////////////
 
 // get the transaction history page's html and put it in a global
 function get_transaction_history_page_html(){
@@ -117,13 +95,18 @@ function get_banner_page(){
     })
     .success(function(return_data){
         banner_page_html = return_data;
-        console.log(banner_page_html);
         banner_page_html_ready = true;
     })
     .error(function(return_data){
         handle_error_getting_html("banner page", return_data);
     });
 }
+
+
+
+////////////////////////////////////////////////////////////////
+// SENDING THE USER PLACES
+////////////////////////////////////////////////////////////////
 
 // wait until our managemyid page AJAX requests are done
 // then send them along to ddstown
@@ -136,7 +119,7 @@ function send_them_to_our_server_with_data_when_ready(waiting_on){
         ready = banner_page_html_ready;
     }
     if(ready){
-        console.log(current_balance_page_html);
+        //console.log(current_balance_page_html);
         send_them_to_our_server_with_data();
     }
     else{
@@ -151,34 +134,39 @@ function send_them_to_our_server_with_data(){
         'transaction_history_html' : transaction_history_page_html,
         'banner_page_html' : banner_page_html,
     };
-    if(DEBUG){
-        console.log(post_data);
-        console.log(main_ddstown_site_url);
-    }
-    else{
-        send_user_by_post(main_ddstown_site_url, post_data);
-    }
-
+    send_user_by_post(main_ddstown_site_url, post_data);
 }
 
-// run right before we start grabbing HTML pages with AJAX
-function say_were_taking_info(){
-    css = ' \
-        body {text-align: center; font-family: helvetica; font-size: 20pt} \
-        h1 {text-align: center;} \
-        h2 {font-size: .8em} \
-        ';
-    html = ' \
-        <h1> \
-        Stand by for DDStown... \
-        </h1> \
-<img src="http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif" \> \
-        <br /> \
-        <h2> \
-        Collecting your DDS usage data... \
-        </h2> \
-        ';
-    show_only_this_html_and_css(html, css);
+// sends the user to the given url, with given data in post
+function send_user_by_post(url, data){
+    if(DEBUG){
+        console.log(data);
+        console.log(url);
+        return false;
+    }
+    form = $('<form method="post" action="' + url + '"></form>');
+    for (var name in data){
+        value = data[name];
+        value = Base64.encode(value);
+        form.append($('<input type="hidden" name="' + name + '" value="' + value + '">'));
+    }
+    form.submit();
+}
+
+function send_them_to_managemyid(){
+    window.location.href = managemyid_main_page;
+}
+
+
+////////////////////////////////////////////////////////////////
+// MANIPULATING THE DOM (COSMETIC STUFF)
+////////////////////////////////////////////////////////////////
+function show_only_this_html_and_css(html, css){
+    css = '<style type="text/css">' + css + '</style>';
+    $(document.body).empty();
+    $("head :not(script)").remove();
+    $(document.head).append($(css));
+    $(document.body).append($(html));
 }
 
 // run when they click the bookmarklet from any page other than managemyid
@@ -205,17 +193,49 @@ function say_were_taking_them_to_managemyid(){
     $('button').click(send_them_to_managemyid);
 }
 
-function send_them_to_managemyid(){
-    window.location.href = managemyid_main_page;
+// run right before we start grabbing HTML pages with AJAX
+function say_were_taking_info(){
+    css = ' \
+        body {text-align: center; font-family: helvetica; font-size: 20pt} \
+        h1 {text-align: center;} \
+        h2 {font-size: .8em} \
+        ';
+    html = ' \
+        <h1> \
+        Stand by for DDStown... \
+        </h1> \
+<img src="http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif" \> \
+        <br /> \
+        <h2> \
+        Collecting your DDS usage data... \
+        </h2> \
+        ';
+    show_only_this_html_and_css(html, css);
 }
 
-function show_only_this_html_and_css(html, css){
-    css = '<style type="text/css">' + css + '</style>';
-    $(document.body).empty();
-    $("head :not(script)").remove();
-    $(document.head).append($(css));
-    $(document.body).append($(html));
+
+
+////////////////////////////////////////////////////////////////
+// CONTROL FLOW
+////////////////////////////////////////////////////////////////
+
+
+// see if an html str is the login page
+function is_login_page(html){
+    return html.indexOf('Forgot My Password') >= 0;
 }
+
+function handle_they_must_log_in(){
+    alert("Woops! You're not logged in. Redirecting you to login.");
+    window.location.href = managemyid_login_page;
+}
+
+function handle_error_getting_html(page, e){
+    error_message_str = e.statusText + ' ' + String(e.status);
+    alert("Error getting " + page + " page! Super lame. Sorry. " + error_message_str);
+    console.log(e);
+}
+
 
 // MAIN
 host = $(location).attr('host');
